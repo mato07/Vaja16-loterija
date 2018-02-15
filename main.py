85a468411c081b755c56b1c7b9f475826cf60d1e
Matija Rifl <matija.rifl@gmail.com>
2018-02-15 18:14:36 +0100

    dodal par stvari

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
-import jinja2
+import jinja2 # lahko so rdeci ker v resnici poganja googlov python
 import webapp2
 
 
@@ -20,7 +21,7 @@ class BaseHandler(webapp2.RequestHandler):
     def render(self, template, **kw):
         return self.write(self.render_str(template, **kw))
 
-    def render_template(self, view_filename, params=None):
+    def render_template(self, view_filename, params=None): # pod params lahko dodamo argumente za spletno stran
         if params is None:
             params = {}
         template = jinja_env.get_template(view_filename)
@@ -29,8 +30,15 @@ class BaseHandler(webapp2.RequestHandler):
 
 class MainHandler(BaseHandler):
     def get(self):
-        return self.render_template("hello.html")
+
+        # tu vpises kodo
+
+        izzrebane_stevilke = 2+3*5
+
+        info = {"sporocilo": izzrebane_stevilke, "pošiljatelj": "Matija", "stvar": "tretja stvar"}
+        return self.render_template("hello.html", info) #Pokazi ta template
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
+    webapp2.Route('/pozdrav', MainHandler)
 ], debug=True)
